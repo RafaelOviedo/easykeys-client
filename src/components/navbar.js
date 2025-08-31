9ef81ff00e34e1ddfcd650867659c40dde822af5
Rafael Oviedo <oviedo.rp@gmail.com>
2025-08-31 13:20:06 -0300

    feat: adds components structure and move navbar to a Class to DRY out the code

---
+++ src/components/navbar.js
@@ -0,0 +1,25 @@
+
+export class Navbar extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+      <nav class="navbar">
+        <div class="logo-container">
+          <img class="keyboard-image" src="../../assets/images/keyboard-image.png" />
+          <span class="logo-text">EasyKeys</span>
+        </div>
+
+        <div class="navbar-buttons-container">
+          <div class="burger-menu-container">
+            <div class="burger-line"></div>
+            <div class="burger-line"></div>
+            <div class="burger-line"></div>
+          </div>
+
+          <a class="cart-container" href="../CartPage/CartPage.html">
+            <img class="cart-image" src="../../assets/images/icon-cart.png" />
+          </a>
+        </div>
+      </nav>`;
+  }
+}
+customElements.define("ek-navbar", Navbar);
