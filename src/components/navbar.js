5ca65d6b5e8ca876d979e756f12091eac13ba68a
Rafael Oviedo <oviedo.rp@gmail.com>
2025-09-13 15:43:31 -0300

    feat: adds logic to open navbar menu on mobile screen

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,6 +1,6 @@
 
 export class Navbar extends HTMLElement {
-  connectedCallback() {
+  renderNavbar() {
     this.innerHTML = `
       <nav class="navbar">
         <div class="logo-container">
@@ -20,6 +20,42 @@ export class Navbar extends HTMLElement {
           </a>
         </div>
       </nav>`;
+
+    this.querySelector('.burger-menu-container').addEventListener('click', () => {
+      this.renderOpenNavbar();
+    });
+  }
+
+  renderOpenNavbar() {
+    this.innerHTML = `
+      <nav class="opened-navbar-container">
+        <nav class="opened-navbar">
+          <div class="logo-container">
+            <img class="keyboard-image" src="../../assets/images/keyboard-image.png" />
+            <a class="logo-text" href="../../index.html">EasyKeys</a>
+          </div>
+
+          <div class="close-menu-container">
+            <div class="close-button">
+              &#10005;
+            </div>
+          </div>
+        </nav>
+
+        <div class="navbar-options-menu">
+          <a class="navbar-option" href="../../index.html">Home</a>
+          <a class="navbar-option" href="../../src/KeyboardsPage/KeyboardsPage.html">Keyboards</a>
+          <a class="navbar-option" href="../../src/AboutPage/AboutPage.html">About us</a>
+        </div>
+      </nav>`;
+
+    this.querySelector('.close-menu-container').addEventListener('click', () => {
+      this.renderNavbar();
+    });
+  }
+
+  connectedCallback() {
+    this.renderNavbar();
   }
 }
 
